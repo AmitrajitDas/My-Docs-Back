913642f32c8525f838687a923b1348078dfc8bd4
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-23 22:17:38 +0530

    setting up sockets in server side

--- server.js
+++ server.js
@@ -6,5 +6,7 @@ const io = require("socket.io")(3001, {
 })
 
 io.on("connection", socket => {
-    console.log("connected")
+    socket.on("send-changes", delta => {
+        socket.broadcast.emit("recieve-changes", delta)
+    })
 })
