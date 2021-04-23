b6c8c83abaabc7303ded4fa8611965520e802354
Amitrajit31 <amitrajitdas31@gmail.com>
2021-04-23 23:13:08 +0530

    handling document on server side

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const io = require("socket.io")(3001, {
 })
 
 io.on("connection", socket => {
-    socket.on("send-changes", delta => {
-        socket.broadcast.emit("recieve-changes", delta)
+    socket.on("get-document", documentId => {
+        const data = ""
+        socket.join(documentId);
+        socket.emit("load-document", data);
+
+        socket.on("send-changes", delta => {
+            socket.broadcast.to(documentId).emit("recieve-changes", delta)
+         })
     })
 })
